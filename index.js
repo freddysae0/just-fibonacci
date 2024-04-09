6d208f45daa8e904bcd4df4a7c73e8e92329a1d9
freddysae0 <freddysae00@gmail.com>
2024-04-09 04:13:29 -0400

    Función generadora

---
+++ index.js
@@ -0,0 +1,3 @@
+import { fibonacci as generatorFunction } from "./ways-to-generate/gen-function.js";
+
+export { generatorFunction };
---
+++ ways-to-generate/gen-function.js
@@ -0,0 +1,27 @@
+/* #1 Función generadora 
+Antes puedes informarte sobre estas funciones acá:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
+*/
+// Inicialización de las variables que almacenarán los dos primeros números de la secuencia de Fibonacci.
+let a = 0;
+let b = 1;
+
+/**
+ * Genera números de la secuencia de Fibonacci de manera infinita.
+ * Esta función no toma argumentos y utiliza un bucle infinito para generar los números de Fibonacci.
+ * @returns {Generator} Un generador que produce números de la secuencia de Fibonacci.
+ */
+function* fibonacci() {
+  // Bucle infinito que permite la generación continua de números de Fibonacci.
+  while (true) {
+    // La palabra clave 'yield' pausa la ejecución de la función y devuelve el valor actual de 'a', que es el próximo número en la secuencia de Fibonacci.
+    yield a;
+    // Cálculo del siguiente número en la secuencia de Fibonacci.
+    let c = a + b;
+    // Actualización de las variables 'a' y 'b' para preparar la generación del siguiente número en la secuencia.
+    a = b;
+    b = c;
+  }
+}
+
+export { fibonacci };
