dcb04604dbe116aafbd8c9da284ce7e2cac00da0
Freddy <freddysae00@gmail.com>
2024-04-09 17:45:56 -0400

    Update gen-function.js

--- ways-to-generate/gen-function.js
+++ ways-to-generate/gen-function.js
@@ -2,7 +2,8 @@
 Antes puedes informarte sobre estas funciones acá:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
 */
-// Inicialización de las variables que almacenarán los dos primeros números de la secuencia de Fibonacci.
+// Inicialización de las variables que almacenarán los dos 
+// primeros números de la secuencia de Fibonacci.
 let a = 0;
 let b = 1;
 
@@ -14,11 +15,13 @@ let b = 1;
 function* genFunction() {
   // Bucle infinito que permite la generación continua de números de Fibonacci.
   while (true) {
-    // La palabra clave 'yield' pausa la ejecución de la función y devuelve el valor actual de 'a', que es el próximo número en la secuencia de Fibonacci.
+    // La palabra clave 'yield' pausa la ejecución de la función y devuelve
+    // el valor actual de 'a', que es el próximo número en la secuencia de Fibonacci.
     yield a;
     // Cálculo del siguiente número en la secuencia de Fibonacci.
     let c = a + b;
-    // Actualización de las variables 'a' y 'b' para preparar la generación del siguiente número en la secuencia.
+    // Actualización de las variables 'a' y 'b' para preparar 
+    // la generación del siguiente número en la secuencia.
     a = b;
     b = c;
   }
